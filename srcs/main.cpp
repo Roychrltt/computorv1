a32934b97ea2790b86c781dfeb14283083ce060c
Xiaoyun Xu <xiaxu@f1r11s17.clusters.42paris.fr>
2025-11-08 16:01:27 +0100

    parsing done with regex

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,5 +10,25 @@ int main(int argc, char** argv)
 	}
 
 	std::string s(argv[1]);
+	std::regex term("([+-]?\\s*\\d*\\.?\\d*)\\s*\\*\\s*X\\^(\\d+)");
+	std::map<int, double> coef;
+	std::string left = s.substr(0, s.find('='));
+	std::string right = s.substr(s.find('=') + 1);
+
+	auto parse = [&](std::string part, int sign) {
+		for (auto it = std::sregex_iterator(part.begin(), part.end(), term); it != std::sregex_iterator(); ++it) {
+			std::string a = (*it)[1].str();
+			a.erase(remove(a.begin(), a.end(), ' '), a.end());
+			if (a == "+" || a == "" || a == "+.") a = "1";
+			else if (a == "-") a = "-1";
+			double c = std::stod(a);
+			int p = std::stoi((*it)[2]);
+			coef[p] += c * sign;
+		}
+	};
+
+	parse(left, 1);
+	parse(right, -1);
+
 	__Made in France__
 }
